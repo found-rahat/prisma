22e5b7a1884ef08cf93e49b5e746ff7b3e92bb7f
found-rahat <206825051+irahat120@users.noreply.github.com>
2025-08-18 14:16:15 +0600

    default not found page create

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">404 | Not Found</h1>
+        <p className="mt-4 text-lg">Could not page resource</p>
+      </div>
+    </div>
+  );
+}
