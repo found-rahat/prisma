0f13153553449ba2da9ece3511a434cb6a5c2ca6
found-rahat <206825051+irahat120@users.noreply.github.com>
2025-08-20 10:27:33 +0600

    loading

---
+++ src/app/insert/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+  return (
+    <div className="p-6 max-w-md mx-auto">
+      <div className="animate-pulse space-y-4">
+        <div className="h-6 bg-gray-300 rounded w-3/4"></div>
+        <div className="h-4 bg-gray-300 rounded"></div>
+        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+        <div className="h-4 bg-gray-300 rounded w-2/3"></div>
+      </div>
+    </div>
+  );
+}
--- src/app/insert/page.tsx
+++ src/app/insert/page.tsx
@@ -2,11 +2,17 @@ import Image from "next/image";
 import prisma from "../../../prisma/prisma";
 import AddFormDesign from "./AddFormDesign";
 import Menu from "../menu";
+import { resolve } from "path";
 export const metadata = {
   title: "Insert User data",
 };
 
 export default async function Home() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("InterNational delay");
+    }, 2000);
+  });
   return (
     <main>
       <Menu />
