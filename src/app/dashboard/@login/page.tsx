e155fdae1ae10c12004d29753ee8ff4aaed3e6ca
found-rahat <206825051+irahat120@users.noreply.github.com>
2025-08-20 11:48:48 +0600

    conditional route
    ami chaile route a condiation o dte pari. example jokhon kuno route ami outside ar person k access korte na o dte pari tokhon ami ai conditional route use korte pari

---
+++ src/app/dashboard/@login/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/card";
+import Menu from "../../menu";
+
+export default function LoginSlot() {
+  return (
+    <div>
+      <Menu />
+      <Card>Please Login then Access it</Card>;
+    </div>
+  );
+}
