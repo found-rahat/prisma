bd482109c5cb2aba960bb29609239cf209195356
found-rahat <206825051+irahat120@users.noreply.github.com>
2025-08-21 14:06:05 +0600

    GET handler

---
+++ src/app/comment/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+  {
+    id: 1,
+    comment: "this is the first comments",
+  },
+  {
+    id: 2,
+    comment: "this is the second comments",
+  },
+  {
+    id: 3,
+    comment: "this is the third comments",
+  },
+  {
+    id: 4,
+    comment: "this is the forth comments",
+  },
+];
