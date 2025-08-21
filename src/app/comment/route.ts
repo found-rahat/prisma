012aa3dc0230c82faf7445fac063256955c4b39c
found-rahat <206825051+irahat120@users.noreply.github.com>
2025-08-21 15:04:02 +0600

    Post Handler

--- src/app/comment/route.ts
+++ src/app/comment/route.ts
@@ -3,3 +3,31 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(req: Request) {
+  const comment = await req.json();
+
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    status: 201,
+  });
+}
+
+// export async function POST(req: Request) {
+//   const body = await req.json();
+
+//   const newComment = {
+//     id: comments.length + 1,
+//     text: body.text,
+//   };
+
+//   comments.push(newComment);
+//   return new Response(JSON.stringify(newComment), {
+//     status: 201,
+//     headers: { "Content-Type": "application/json" },
+//   });
+// }
