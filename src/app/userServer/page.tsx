5242badf8e03e5476b3c2b5a9a3d0eb5d0f61bb2
found-rahat <206825051+irahat120@users.noreply.github.com>
2025-08-24 18:01:51 +0600

    Loading and Error States for user server fatch

--- src/app/userServer/page.tsx
+++ src/app/userServer/page.tsx
@@ -9,6 +9,7 @@ type User = {
 };
 
 export default async function userServer() {
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await response.json();
   console.log(users);
